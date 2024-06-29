66e3f47a77b87e7c321d389bae65f23376452154
hima890 <hfibrahim90@gmail.com>
2024-06-29 10:28:35 +0200

    Update/Config-file-WiFi-add-virtualEnEnv

---
+++ ESP32-Code/config.py
@@ -0,0 +1,5 @@
+# Configuration file for pin assignments and other constants
+
+# Wi-Fi settings
+WIFI_SSID = 'Wokwi-GUEST'
+WIFI_PASSWORD = ''
---
+++ ESP32-Code/wifi_setup.py
@@ -0,0 +1,13 @@
+import network
+import time
+from config import WIFI_PASSWORD, WIFI_SSID
+
+
+print("Connecting to WiFi", end="")
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+sta_if.connect('Wokwi-GUEST', '')
+while not sta_if.isconnected():
+  print(".", end="")
+  time.sleep(0.1)
+print(" Connected!")
