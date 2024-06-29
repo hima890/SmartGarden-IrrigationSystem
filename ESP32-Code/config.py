2067d0437bcdb0761a6e35eaa8989d112f8af5d7
hima890 <hfibrahim90@gmail.com>
2024-06-29 11:09:36 +0200

    Update/Config-file-Wifi-file

--- ESP32-Code/config.py
+++ ESP32-Code/config.py
@@ -3,3 +3,12 @@
 # Wi-Fi settings
 WIFI_SSID = 'Wokwi-GUEST'
 WIFI_PASSWORD = ''
+
+# Sensors pin assignments
+SOIL_MOISTURE_1_PIN = 32
+SOIL_MOISTURE_2_PIN = 33
+SOIL_MOISTURE_3_PIN = 34
+SOIL_MOISTURE_4_PIN = 35
+
+# Valve pin
+VALVE_PIN = 25
