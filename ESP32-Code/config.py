63319d488c7b5bae425315fa5e1470c4188a0e99
hima890 <hfibrahim90@gmail.com>
2024-06-30 08:06:44 +0200

    Update/Config-file-MQTT-settings

--- ESP32-Code/config.py
+++ ESP32-Code/config.py
@@ -12,3 +12,11 @@ SOIL_MOISTURE_4_PIN = 35
 
 # Valve pin
 VALVE_PIN = 25
+
+# MQTT settings
+MQTT_BROKER = 'bcfe325a54b74177a07b98e7235cb5ea.s1.eu.hivemq.cloud'
+MQTT_PORT = 8883
+MQTT_CLIENT_ID_ESP32 = 'ESP32_Client'
+MQTT_CLIENT_ID_FLASK = 'Flask_Server'
+MQTT_TOPIC_SENSOR = 'sensor/moisture'
+MQTT_TOPIC_CONTROL = 'valve/control'
---
+++ ESP32-Code/mqtt_client.py
@@ -0,0 +1,4 @@
+""" MQTT client for sending sensor data, calculating averages,
+and responding to commands
+"""
+import time
