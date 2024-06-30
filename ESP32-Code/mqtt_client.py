fc33681073bb41e0324bac1ea9bfec3d2f53d35e
hima890 <hfibrahim90@gmail.com>
2024-06-30 09:50:10 +0200

    Update/On_message_function-adding-on-message-function+refactorring-mqtt_client

--- ESP32-Code/mqtt_client.py
+++ ESP32-Code/mqtt_client.py
@@ -6,10 +6,12 @@ import ujson
 from umqtt.simple import MQTTClient
 from sensors import SoilMoistureSensor
 from pump import Pump
+from utility.on_message_function import on_message
 from config import (MQTT_CLIENT_ID_ESP32,
                     MQTT_BROKER,
                     MQTT_PORT,
                     MQTT_TOPIC_CONTROL)
+
 
 # Initialize sensor classe
 soil_moisture_sensor = SoilMoistureSensor()
@@ -20,35 +22,33 @@ sensors_data_avreg = soil_moisture_sensor.avg_sensor_data()
 pump = Pump()
 pump_status = pump.status
 
-# MQTT Callback
-def on_message(topic, msg):
-    """
-    Control the pump status based on the MQTT message.
-
-    Args:
-        topic(object): Un umqtt object message was published to.
-        msg(object):  Un umqtt object that contains:
-            msg.topic(object): The topic that the message was published to.
-            msg.payload(object): The actual message payload.
-
-    Returns:
-        No return.
-    """
-    # Echo the topic and the msg data
-    print((topic, msg))
-    # Check the message and change the pump status
-    if msg == b'open_valve':
-        pump.open()
-    elif msg == b'close_valve':
-        pump.close()
-
 # MQTT Setup
 client = MQTTClient(
     MQTT_CLIENT_ID_ESP32,
     MQTT_BROKER,
     port=MQTT_PORT
     )
-client.set_callback(on_message)
+
+# MQTT Callback Setup
+# Wrapper Function (on_message_callback)
+def on_message_callback(topic, msg):
+    """
+        A new function on_message_callback that takes topic
+        and msg as parameters (as expected by the MQTT client)
+        and calls on_message with the valve object.
+
+        Args:
+            topic (bytes): The topic that the message was published to.
+            msg (bytes): The actual message payload.
+
+        Returns:
+            No return.
+    """
+    on_message(topic, msg, pump)
+
+# Set the callback to the wrapper Function
+client.set_callback(on_message_callback)
+
 
 # Try to connect to the MQTT server
 try:
